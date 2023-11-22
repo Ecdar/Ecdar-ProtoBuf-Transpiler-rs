f69812a7d87e551dd213454cb0d6e2f07fc738c5
KamyaPA <peteraksel@live.dk>
2023-11-22 10:52:10 +0100

    changed to quote

--- ecdar-protobuf-transpiler/src/lib.rs
+++ ecdar-protobuf-transpiler/src/lib.rs
@@ -1,4 +1,6 @@
 use convert_case::*;
+use proc_macro2::TokenStream;
+use quote::*;
 
 mod services {
     ecdar_protobuf_transpiler_macros::get_services!();
@@ -24,13 +26,14 @@ impl services::ProtobuffTypes {
 }
 
 pub struct CompileVariables {
-    pub fn_name: String,
-    pub endpoint_name: String,
-    pub service_name: String,
-    pub in_struct: String,
-    pub in_struct_name: String,
+    pub fn_name: TokenStream,
+    pub endpoint_name: TokenStream,
+    pub service_name: TokenStream,
+    pub in_struct: TokenStream,
+    pub in_struct_name: TokenStream,
     pub in_struct_has_body: bool,
-    pub rtn_struct: String,
+    pub rtn_struct: TokenStream,
+    pub client : TokenStream,
 }
 
 pub fn compile<T>(foreach: impl Fn(CompileVariables) -> T) -> Vec<T> {
@@ -41,27 +44,47 @@ pub fn compile<T>(foreach: impl Fn(CompileVariables) -> T) -> Vec<T> {
                 .endpoints
                 .iter()
                 .map(|endpoint| {
-                    let fn_name = get_fn_name(service.name, endpoint.name).to_case(Case::Snake);
-                    let in_struct_has_body = endpoint.input_type.to_rust_type() != "()";
-                    let in_struct_name = format!("In{}", fn_name.to_case(Case::Pascal));
-                    let rtn_struct = endpoint.output_type.to_rust_type();
+                    let fn_name = format_ident!("{}", get_fn_name(service.name, endpoint.name));
+                    let in_struct_name = format_ident!("In{}",
+                        service.name.to_case(Case::Pascal) + 
+                        endpoint.name.to_case(Case::Pascal).as_str()
+                    );
+                    let body_rust_type_str = endpoint.input_type.to_rust_type();
+                    let in_struct_has_body = body_rust_type_str != "()";
+
+                    let body = if in_struct_has_body {
+                        let body_rust_type = format_ident!("{}", body_rust_type_str);
+                        quote!{ pub body : #body_rust_type}
+                    } else {
+                        quote!{}
+                    };
+
+                    let rtn_struct = format_ident!("{}", endpoint.output_type.to_rust_type());
+
+                    let client = format_ident!(
+                        "services::{}_client::{}Client",
+                        service.name.to_case(Case::Snake),
+                        service.name.to_case(Case::Pascal)
+                    );
+
+                    let endpoint_name = format_ident!("{}", endpoint.name);
+                    let service_name = format_ident!("{}", service.name);
 
                     foreach(CompileVariables {
-                        endpoint_name: endpoint.name.into(),
-                        service_name: service.name.into(),
-                        in_struct: format!(
-                            "struct {}{{ ip : String, {}}}",
-                            in_struct_name,
-                            if in_struct_has_body {
-                                format!("body : {}", endpoint.input_type.to_rust_type())
-                            } else {
-                                "".into()
-                            }
-                        ),
-                        in_struct_has_body,
-                        in_struct_name,
-                        rtn_struct,
-                        fn_name,
+                        endpoint_name : quote!(#endpoint_name),
+                        service_name: quote!(#service_name),
+                        fn_name : quote!(#fn_name),
+                        in_struct : quote!{
+                            #[derive(serde::Serialize, serde::Deserialize)]
+                            pub struct #in_struct_name {
+                                pub ip : String,
+                                #body
+                            } 
+                        },
+                        in_struct_name : quote!(#in_struct_name),
+                        client : quote!(#client),
+                        rtn_struct : quote!(#rtn_struct),
+                        in_struct_has_body
                     })
                 })
                 .collect::<Vec<_>>()
