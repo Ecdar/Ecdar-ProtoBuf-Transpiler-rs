31511d6bd1c366691dd5156c399d303a998d9701
Thomas Lohse <49527735+t-lohse@users.noreply.github.com>
2024-03-13 16:12:37 +0100

    SW5 changes (#1)

--- ecdar-protobuf-transpiler/src/lib.rs
+++ ecdar-protobuf-transpiler/src/lib.rs
@@ -6,7 +6,7 @@ mod services {
     ecdar_protobuf_transpiler_macros::get_services!();
 }
 
-impl services::ProtobuffTypes {
+impl ProtobuffTypes {
     pub fn to_rust_type(&self) -> TokenStream {
         let is_google = self.name.split('.').next().unwrap() == "google";
         match self.name {
@@ -15,7 +15,7 @@ impl services::ProtobuffTypes {
                 quote!(
                     ecdar_protobuf::services::#ident
                 )
-            },
+            }
             "google.protobuf.Empty" => quote!(()),
             _ => {
                 panic!(
@@ -35,11 +35,11 @@ pub struct CompileVariables {
     pub in_struct_name: TokenStream,
     pub in_struct_has_body: bool,
     pub rtn_struct: TokenStream,
-    pub client : TokenStream,
+    pub client: TokenStream,
 }
 
 pub fn compile<T>(foreach: impl Fn(CompileVariables) -> T) -> Vec<T> {
-    services::SERVICES
+    SERVICES
         .iter()
         .map(|service| {
             service
@@ -47,50 +47,54 @@ pub fn compile<T>(foreach: impl Fn(CompileVariables) -> T) -> Vec<T> {
                 .iter()
                 .map(|endpoint| {
                     let fn_name = format_ident!("{}", get_fn_name(service.name, endpoint.name));
-                    let in_struct_name = format_ident!("In{}{}",
-                        service.name.to_case(Case::Pascal), 
+                    let in_struct_name = format_ident!(
+                        "In{}{}",
+                        service.name.to_case(Case::Pascal),
                         endpoint.name.to_case(Case::Pascal)
                     );
                     let body_rust_type = endpoint.input_type.to_rust_type();
-                    let in_struct_has_body = body_rust_type.to_string() != "()".to_string();
+                    let in_struct_has_body = body_rust_type.to_string() != "()";
 
                     let body = if in_struct_has_body {
-                        quote!{ pub body : #body_rust_type}
+                        quote! { pub body : #body_rust_type}
                     } else {
-                        quote!{}
+                        quote! {}
                     };
 
                     let rtn_struct = endpoint.output_type.to_rust_type();
 
-                    let client_module = format_ident!("{}_client", service.name.to_case(Case::Snake));
-                    let client_struct = format_ident!("{}Client", service.name.to_case(Case::Pascal));
-
+                    let client_module =
+                        format_ident!("{}_client", service.name.to_case(Case::Snake));
+                    let client_struct =
+                        format_ident!("{}Client", service.name.to_case(Case::Pascal));
 
                     let endpoint_name = format_ident!("{}", endpoint.name.to_case(Case::Snake));
                     let service_name = format_ident!("{}", service.name.to_case(Case::Snake));
 
                     foreach(CompileVariables {
-                        endpoint_name : quote!(#endpoint_name),
+                        endpoint_name: quote!(#endpoint_name),
                         service_name: quote!(#service_name),
-                        fn_name : quote!(#fn_name),
-                        in_struct : quote!{
+                        fn_name: quote!(#fn_name),
+                        in_struct: quote! {
                             #[derive(serde::Serialize, serde::Deserialize)]
                             pub struct #in_struct_name {
                                 pub ip : String,
                                 #body
-                            } 
+                            }
                         },
-                        in_struct_name : quote!(#in_struct_name),
-                        client : quote!(ecdar_protobuf::services::#client_module::#client_struct),
+                        in_struct_name: quote!(#in_struct_name),
+                        client: quote!(ecdar_protobuf::services::#client_module::#client_struct),
                         rtn_struct,
-                        in_struct_has_body
+                        in_struct_has_body,
                     })
                 })
                 .collect::<Vec<_>>()
-        }).reduce(|mut acc, mut v| {
+        })
+        .reduce(|mut acc, mut v| {
             acc.append(&mut v);
             acc
-        }).unwrap()
+        })
+        .unwrap()
 }
 
 fn get_fn_name(service_name: &str, enpoint_name: &str) -> String {
